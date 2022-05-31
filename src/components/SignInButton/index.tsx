07ef0cc330e7f9f211d7639c237066b3da5df01f
Thiago Nunes <msndothiago@live.com>
2022-05-31 20:10:12 -0300

    api commit

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,22 @@
 import styles from './styles.module.scss';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, useSession, signOut } from "next-auth/react";
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const { data: session } = useSession()
+
+    console.log(session);
     
-    return isUserLoggedIn ? (
+    return session ? (
         <button 
             type="button"
             className={styles.signInButton}
+            onClick={() => signOut()}
         >
             
             <FaGithub color='#04d361' />
-            Thiago Nunes
+            {session.user.name}
             <FiX color='#737380' className={styles.closeIcon} />
         </button>
 
@@ -20,6 +24,7 @@ export function SignInButton() {
         <button 
             type="button"
             className={styles.signInButton}
+            onClick={() => signIn('github')}
         >
             
             <FaGithub color='#eba417' />
